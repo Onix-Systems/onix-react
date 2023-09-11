eae86d7c80d12c74cf3943efcef9bf2576e068fb
Nastya Lytvyn <109170684+goodcodd@users.noreply.github.com>
2023-09-11 15:08:26 +0300

    Create NavigationContainer.jsx

---
+++ examples/Navigation/NavigationContainer.jsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      {/* Rest of your app code */}
+    </NavigationContainer>
+  );
+};
+
+export default App;
