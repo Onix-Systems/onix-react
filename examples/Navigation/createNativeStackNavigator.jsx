9b570f713cb077275b05b5dc7fa8e6f16fca3ea9
Nastya Lytvyn <109170684+goodcodd@users.noreply.github.com>
2023-09-11 15:09:06 +0300

    Create createNativeStackNavigator.jsx

---
+++ examples/Navigation/createNativeStackNavigator.jsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
+
+const MyStack = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{title: 'Welcome'}}
+        />
+        <Stack.Screen name="Profile" component={ProfileScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
