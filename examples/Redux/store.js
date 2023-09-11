68d730e40a20656a183f25779c7e1c921ed40c99
Nastya Lytvyn <109170684+goodcodd@users.noreply.github.com>
2023-09-11 15:17:46 +0300

    Create store.js

---
+++ examples/Redux/store.js
@@ -0,0 +1,21 @@
+// store.js
+import { createStore } from 'redux';
+
+const initialState = {
+  count: 0,
+};
+
+const counterReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return { count: state.count + 1 };
+    case 'DECREMENT':
+      return { count: state.count - 1 };
+    default:
+      return state;
+  }
+};
+
+const store = createStore(counterReducer);
+
+export default store;
