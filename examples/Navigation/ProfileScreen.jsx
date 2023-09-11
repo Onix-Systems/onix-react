13b486e5173f643e36abcf6060a83344ea1341aa
Nastya Lytvyn <109170684+goodcodd@users.noreply.github.com>
2023-09-11 15:10:10 +0300

    Create ProfileScreen.jsx

---
+++ examples/Navigation/ProfileScreen.jsx
@@ -0,0 +1,13 @@
+const HomeScreen = ({navigation}) => {
+  return (
+    <Button
+      title="Go to Jane's profile"
+      onPress={() =>
+        navigation.navigate('Profile', {name: 'Jane'})
+      }
+    />
+  );
+};
+const ProfileScreen = ({navigation, route}) => {
+  return <Text>This is {route.params.name}'s profile</Text>;
+};
