35c3460183676a340407bec7d60523746368afc8
Nastya Lytvyn <109170684+goodcodd@users.noreply.github.com>
2023-09-13 13:48:36 +0300

    Create Timer.jsx

---
+++ examples/IntroducingHooks/Timer.jsx
@@ -0,0 +1,14 @@
+import { useState, useEffect } from "react";
+import ReactDOM from "react-dom/client";
+
+function Timer() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setCount((count) => count + 1);
+    }, 1000);
+  });
+
+  return <h1>I've rendered {count} times!</h1>;
+}
