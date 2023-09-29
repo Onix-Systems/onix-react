e49edd80916a7a5ffd59521d0c58168500cdcd77
Nastya Lytvyn <109170684+goodcodd@users.noreply.github.com>
2023-09-29 15:37:13 +0300

    Create Express.js

---
+++ CORS/next.js/Express.js
@@ -0,0 +1,16 @@
+//install
+// $ npm install cors
+
+const express = require('express')
+const cors = require('cors')
+const app = express()
+
+app.use(cors())
+
+app.get('/products/:id', function (req, res, next) {
+  res.json({msg: 'This is CORS-enabled for all origins!'})
+})
+
+app.listen(80, function () {
+  console.log('CORS-enabled web server listening on port 80')
+})
