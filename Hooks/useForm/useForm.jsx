3b1319f6e8cccaace5f34e05b64a8733343f86b7
Nastya Lytvyn <109170684+goodcodd@users.noreply.github.com>
2023-10-03 16:31:08 +0300

    Create useForm.jsx

---
+++ Hooks/useForm/useForm.jsx
@@ -0,0 +1,23 @@
+// install
+// npm install react-hook-form
+
+import { useForm } from 'react-hook-form';
+
+function App() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  return (
+    <form onSubmit={handleSubmit((data) => console.log(data))}>
+      <input {...register('firstName')} />
+      <input {...register('lastName', { required: true })} />
+      {errors.lastName && <p>Last name is required.</p>}
+      <input {...register('age', { pattern: /\d+/ })} />
+      {errors.age && <p>Please enter number for age.</p>}
+      <input type="submit" />
+    </form>
+  );
+}
