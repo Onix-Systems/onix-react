7e7d9903b4034b27e37bc3b1f2b1d8e3ccab6fac
Nastya Lytvyn <109170684+goodcodd@users.noreply.github.com>
2023-10-11 16:09:01 +0300

    Update App.js

--- Lazy Loading/App.js
+++ Lazy Loading/App.js
@@ -1,3 +1,4 @@
+// Implement Lazy Loading with React.lazy
 import React, { Suspense } from 'react';
 
 const LazyComponent = React.lazy(() => import('./LazyComponent'));
@@ -7,6 +8,25 @@ const App: React.FC = () => {
     <div>
       <h1>Welcome to My App</h1>
       <Suspense fallback={<div>Loading...</div>}>
+        <LazyComponent />
+      </Suspense>
+    </div>
+  );
+};
+
+export default App;
+ 
+
+// Handle Loading and Error States
+import React, { Suspense } from 'react';
+
+const LazyComponent = React.lazy(() => import('./LazyComponent'));
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <h1>Welcome to My App</h1>
+      <Suspense fallback={<div>Loading...</div>} onError={<div>Error!</div>}>
         <LazyComponent />
       </Suspense>
     </div>
