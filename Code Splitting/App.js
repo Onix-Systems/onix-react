26d6a4ad1cb61a1145135c017300cf7623d87aa4
Nastya Lytvyn <109170684+goodcodd@users.noreply.github.com>
2023-10-12 16:21:39 +0300

    Create App.js

---
+++ Code Splitting/App.js
@@ -0,0 +1,14 @@
+import React, { lazy, Suspense } from 'react';
+
+const LazyComponent = lazy(() => import('./LazyComponent'));
+
+const App = () => (
+  <div>
+    <h1>My React App</h1>
+    <Suspense fallback={<div>Loading...</div>}>
+      <LazyComponent />
+    </Suspense>
+  </div>
+);
+
+export default App;
