eb8bd29b2916ff3258bf34a0d3967ac3f6e1f7f2
Nastya Lytvyn <109170684+goodcodd@users.noreply.github.com>
2023-12-15 17:17:51 +0200

    Update App.js

--- Hooks/useCopyToClipboard/App.js
+++ Hooks/useCopyToClipboard/App.js
@@ -2,11 +2,11 @@ import React from 'react'
 import Component from './Component'
 
 function App() {
-	return (
-		<div className="App">
-			<Component />
-		</div>
-	)
+    return (
+	<div className="App">
+		<Component />
+	</div>
+    )
 }
 
 export default App
