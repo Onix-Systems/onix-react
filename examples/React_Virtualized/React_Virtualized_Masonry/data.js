3b0203661961041667b10f221a567b97d7264953
Anastasiia Lytvyn <109170684+goodcodd@users.noreply.github.com>
2024-06-25 10:29:56 +0300

    Create data.js

---
+++ examples/React_Virtualized/React_Virtualized_Masonry/data.js
@@ -0,0 +1,91 @@
+export default [
+  {
+    image:
+      "https://brand.okstate.edu/site-files/images/brand-guide/colors-orange.png",
+    title: "1",
+  },
+  {
+    image:
+      "https://htmlcolorcodes.com/assets/images/colors/bright-orange-color-solid-background-1920x1080.png",
+    title: "2",
+  },
+  {
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRkm2lwRY6GFFh99BdjBXVTWqpMZLfI-NXmsQ&s",
+    title: "3",
+  },
+  {
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRLW6RUQ_MtvHPhloM27ptBljCp48sDvl2dSw&s",
+    title: "4",
+  },
+  {
+    image:
+      "https://htmlcolorcodes.com/assets/images/colors/red-color-solid-background-1920x1080.png",
+    title: "5",
+  },
+  {
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQyMovUQBgEIkT3xLP8SmIZBO3SuetdESA-jkVDG4ZhBfm3sAYTUc3bWTfV7ZJXKwJqZxk&usqp=CAU",
+    title: "6",
+  },
+  {
+    image:
+      "https://htmlcolorcodes.com/assets/images/colors/green-color-solid-background-1920x1080.png",
+    title: "7",
+  },
+  {
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-SUTraETrk8nNW7YS2lICMkRIldcWy1ilSA&s",
+    title: "8",
+  },
+  {
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTgvmQwHu88e6Pku3ZXGMlpKpbMQ6MjnGwBeQ&s",
+    title: "9",
+  },
+  {
+    image:
+      "https://htmlcolorcodes.com/assets/images/colors/green-color-solid-background-1920x1080.png",
+    title: "10",
+  },
+  {
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ5QAKGgQ65mODiEMWeH_qykgaYB97ozSm0mw&s",
+    title: "11",
+  },
+  {
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ5QAKGgQ65mODiEMWeH_qykgaYB97ozSm0mw&s",
+    title: "12",
+  },
+  {
+    image:
+      "https://img.freepik.com/free-vector/pink-abstract-background_698452-1743.jpg?size=626&ext=jpg&ga=GA1.1.1141335507.1718323200&semt=ais_user",
+    title: "13",
+  },
+  {
+    image: "https://www.color-hex.com/palettes/28997.png",
+    title: "14",
+  },
+  {
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_mFHKtpBd4Mh2K_8ibivJmg7QYRjZxyPmtQ&s",
+    title: "15",
+  },
+  {
+    image:
+      "https://htmlcolorcodes.com/assets/images/colors/red-color-solid-background-1920x1080.png",
+    title: "16",
+  },
+  {
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-SUTraETrk8nNW7YS2lICMkRIldcWy1ilSA&s",
+    title: "17",
+  },
+  {
+    image:
+      "https://img.freepik.com/free-vector/pink-abstract-background_698452-1743.jpg?size=626&ext=jpg&ga=GA1.1.1141335507.1718323200&semt=ais_user",
+    title: "18",
+  },
+];
