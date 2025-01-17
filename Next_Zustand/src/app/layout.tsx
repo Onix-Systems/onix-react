09aa114f2654dd0fd227761f5c4de8952278aabc
Anastasiia Lytvyn <109170684+goodcodd@users.noreply.github.com>
2025-01-17 10:42:15 +0200

    Add files via upload

---
+++ Next_Zustand/src/app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Mastering Zustand in Next.js',
+  description: 'Learn how to effectively manage state with Zustand in a Next.js application',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
+
