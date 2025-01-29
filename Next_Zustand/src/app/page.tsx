c4d4a452ea8b505658cf039727b6072bce5f8cc8
Anastasiia Lytvyn <109170684+goodcodd@users.noreply.github.com>
2025-01-29 14:19:56 +0200

    Update page.tsx

--- Next_Zustand/src/app/page.tsx
+++ Next_Zustand/src/app/page.tsx
@@ -4,7 +4,7 @@ import { Button } from "../components/ui/button"
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <h1 className="text-4xl font-bold mb-8">Zustand with Next.js and SSR</h1>
+      <h1 className="text-4xl font-bold mb-8">Zustand with Next.js</h1>
       <div className="flex space-x-4">
         <Link href="/counter">
           <Button>Go to Counter</Button>
