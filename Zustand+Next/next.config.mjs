a7fd239f1629b9d296cc05589ab87c46ad8784cb
Anastasiia Lytvyn <109170684+goodcodd@users.noreply.github.com>
2025-02-25 17:03:37 +0200

    Create next.config.mjs

---
+++ Zustand+Next/next.config.mjs
@@ -0,0 +1,47 @@
+let userConfig = undefined
+try {
+  userConfig = await import('./user-next.config')
+} catch (e) {
+  // ignore error
+}
+
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
+  },
+  experimental: {
+    webpackBuildWorker: true,
+    parallelServerBuildTraces: true,
+    parallelServerCompiles: true,
+  },
+}
+
+mergeConfig(nextConfig, userConfig)
+
+function mergeConfig(nextConfig, userConfig) {
+  if (!userConfig) {
+    return
+  }
+
+  for (const key in userConfig) {
+    if (
+      typeof nextConfig[key] === 'object' &&
+      !Array.isArray(nextConfig[key])
+    ) {
+      nextConfig[key] = {
+        ...nextConfig[key],
+        ...userConfig[key],
+      }
+    } else {
+      nextConfig[key] = userConfig[key]
+    }
+  }
+}
+
+export default nextConfig
