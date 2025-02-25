ad402b95d713f390d85ec583b9255bed7bd3152b
Anastasiia Lytvyn <109170684+goodcodd@users.noreply.github.com>
2025-02-25 15:43:32 +0200

    Delete Zustand+Next/components directory

--- Zustand+Next/components/ui/input.tsx
+++
@@ -1,4 +0,0 @@
-import type React from "react"
-
-export const Input: React.FC<React.InputHTMLAttributes<HTMLInputElement>> = (props) => <input {...props} />
-
--- Zustand+Next/components/ui/textarea.tsx
+++
@@ -1,4 +0,0 @@
-import type React from "react"
-
-export const Textarea: React.FC<React.TextareaHTMLAttributes<HTMLTextAreaElement>> = (props) => <textarea {...props} />
-
